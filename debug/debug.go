dc4917e53d0ce6d9e7c65dbb8693dacb16c6062b
Johannes Eiglsperger <git@remodding.de>
2019-07-28 12:43:57 +0200

    Unit tests for debug module

--- debug/debug.go
+++ debug/debug.go
@@ -10,12 +10,15 @@ import (
 // Debug contains the current state of debug operation
 var Debug bool
 
+// PrintPrefix contains the prefix of debug output lines
+const PrintPrefix = "[eee-safe-debug] "
+
 // Printf wraps fmt.Printf and displays debug messages unless Debug is set to false
 func Printf(format string, values ...interface{}) {
 	if Debug || flag.Lookup("test.v") != nil {
 		if !strings.HasSuffix(format, "\n") {
 			format += "\n"
 		}
-		_, _ = fmt.Fprintf(os.Stderr, "[eee-safe-debug] "+format, values...)
+		_, _ = fmt.Fprintf(os.Stderr, PrintPrefix+format, values...)
 	}
 }
