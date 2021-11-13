d017efc0cc3e626ba5dd0b7ea5bfc24b5ee22498
Johannes Eiglsperger <git@remodding.de>
2021-11-13 17:46:03 +0100

    Adhere error naming convention

--- pkg/threema/user_agent.go
+++ pkg/threema/user_agent.go
@@ -8,7 +8,7 @@ const ValidUserAgentSubstring = "Threema"
 // ValidateUserAgent checks if a user agent meets the requirements
 func ValidateUserAgent(userAgent string) error {
 	if !strings.Contains(userAgent, ValidUserAgentSubstring) {
-		return &InvalidUserAgentError{userAgent}
+		return &ErrInvalidUserAgent{userAgent}
 	}
 
 	return nil
